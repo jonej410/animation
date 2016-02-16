3cd2c74744b0deced44f4b8c611fde3ba38f8488
jonej410 <javariusjones@Javariuss-MBP.wireless.newschool.edu>
2016-02-16 18:20:10 -0500

    Complete

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -2,20 +2,40 @@
   Classwork
 */
 var ballX = 0;
-var speed = 3;
+var speed = 9;
+var ballY = 0
+var speed2 = 9
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
+  if(mouseIsPressed){
+  fill ("pink")
+  }
   background("white");
   ellipse(ballX, height/2, 50,50);
-
-  if (ballX >= width){
-    speed = -3;
+if(mouseIsPressed){
+  fill ("yellow")
   }
+  ellipse(width/2,ballY,50,50);
 
+//ballX
+  if (ballX >= windowWidth){
+    speed = -9;
+  }
+    else if(ballX <= 0){
+      speed = 9
+  }
+      if(ballY >= windowHeight){
+    speed2 = -9
+  }
+    else if(ballY <= 0){
+      speed2 = 10
+
+    }
   ballX = ballX + speed;
+  ballY = ballY + speed2;
 
 }
